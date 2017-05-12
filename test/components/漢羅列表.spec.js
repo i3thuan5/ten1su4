ffe48013c2bd184116b7b190aa982f6990594d53
a8568730 <a8568730@hotmail.com>
2017-05-12 18:21:09 +0800

    新增漢羅列表spec。 spec加上綜合音標範例，複製內容範例。

---
+++ test/components/漢羅列表.spec.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { expect } from "chai";
+import { shallow } from "enzyme";
+import { HanLoTsua } from "demo-ui";
+import 漢羅列表 from "../../src/元素/顯示/漢羅列表";
+
+const setup = (綜合標音 = []) => {
+  const component = shallow(
+    <漢羅列表 綜合標音={綜合標音}/>,
+    );
+  return {
+    component,
+    漢羅逝: component.find(HanLoTsua),
+  };
+};
+
+const 一標音 = [{
+  '分詞': '大-家｜tai-gaˊ 共-下｜kiung-ha 來｜loiˇ',
+  '漢字': '大家 共下 來',
+  '臺灣客話': 'Tai-gaˊ kiung-ha loiˇ',
+}];
+
+describe("Component", () => {
+  describe("漢羅列表", () => {
+    it("renders 1 漢羅一逝", () => {
+      const { 漢羅逝 } = setup(一標音);
+      expect(漢羅逝).to.have.length(1);
+    });
+    it("renders 1 漢羅一逝 with space", () => {
+      const { 漢羅逝 } = setup(一標音);
+      expect(漢羅逝).to.have.length(1);
+    });
+    it("renders 2 漢羅一逝", () => {
+      const { 漢羅逝 } = setup(一標音.concat(一標音));
+      expect(漢羅逝).to.have.length(2);
+    });
+    it("passes props to 漢羅一逝", () => {
+      const { 漢羅逝 } = setup(一標音);
+      expect(漢羅逝.props()).to.eql({
+        臺羅閏號調: "Tai-gaˊ kiung-ha loiˇ",
+        漢字: "大家 共下 來",
+        分詞: "大-家｜tai-gaˊ 共-下｜kiung-ha 來｜loiˇ",
+      });
+    });
+    it("renders none", () => {
+      const { 漢羅逝 } = setup([]);
+      expect(漢羅逝).to.have.length(0);
+    });
+  });
+});
--- test/components/翻譯結果.spec.js
+++ test/components/翻譯結果.spec.js
@@ -15,9 +15,21 @@ let setup = (腔口='四縣腔', 正在查詢=false, 查詢結果={}) => {
   };
 };
 
+const 一標音 = [{
+  分詞: '大-家｜tai-gaˊ 共-下｜kiung-ha 來｜loiˇ',
+  漢字: '大家 共下 來',
+  臺灣客話: 'Tai-gaˊ kiung-ha loiˇ',
+}];
+
+const 一標音複製內容 = {
+  漢字: '大家共下來',
+  臺羅: 'Tai-gaˊ kiung-ha loiˇ',
+  漢字臺羅: "大家共下來\nTai-gaˊ kiung-ha loiˇ",
+};
+
 describe('元素', ()=> {
-  describe('翻譯結果', ()=> {
-    it('shows error info', ()=> {
+describe('翻譯結果', ()=> {
+  it('shows error info', ()=> {
       const { header } = setup('四縣腔', false, {
         '發生錯誤': true,
         '分詞': '',
@@ -26,17 +38,8 @@ describe('元素', ()=> {
       expect(header).to.have.length(1);
       expect(header.text()).match(/^主機發生錯誤/);
     });
-    it("計算複製內容", () => {
-    const 綜合標音 = [{
-      漢字: "1",
-      臺灣客話: "a",
-    }];
-    const expectResult = {
-      漢字: "1",
-      臺羅: "a",
-      漢字臺羅: "1\na",
-    };
-    expect(計算複製內容(綜合標音)).to.eql(expectResult);
+  it("計算複製內容", () => {
+    expect(計算複製內容(一標音)).to.eql(一標音複製內容);
   });
   it("計算空複製內容", () => {
     const 綜合標音 = [];
@@ -44,44 +47,23 @@ describe('元素', ()=> {
     expect(計算複製內容(綜合標音)).to.eql(expectResult);
   });
   it("計算多行複製內容", () => {
-    const 綜合標音 = [{
-      漢字: "1",
-      臺灣客話: "a",
-    }, {
-      漢字: "2",
-      臺灣客話: "b",
-    }];
+    const 綜合標音 = 一標音.concat(一標音);
     const expectResult = {
-      漢字: "1\n2",
-      臺羅: "a\nb",
-      漢字臺羅: "1\na\n2\nb",
+      漢字: "大家共下來\n大家共下來",
+      臺羅: "Tai-gaˊ kiung-ha loiˇ\nTai-gaˊ kiung-ha loiˇ",
+      漢字臺羅: "大家共下來\nTai-gaˊ kiung-ha loiˇ\n大家共下來\nTai-gaˊ kiung-ha loiˇ",
     };
     expect(計算複製內容(綜合標音)).to.eql(expectResult);
   });
   it("移除漢字之間空白", () => {
-    const 綜合標音 = [{
-      漢字: "大家 共下 來",
-      臺灣客話: "Tai-gaˊ kiung-ha loiˇ",
-    }];
-    const expectResult = {
-      漢字: "大家共下來",
-      臺羅: "Tai-gaˊ kiung-ha loiˇ",
-      漢字臺羅: "大家共下來\nTai-gaˊ kiung-ha loiˇ",
-    };
-    expect(計算複製內容(綜合標音)).to.eql(expectResult);
+    expect(計算複製內容(一標音)).to.eql(一標音複製內容);
   });
   it("移除多行漢字之間空白", () => {
-    const 綜合標音 = [{
-      漢字: "大家 共下 來",
-      臺灣客話: "Tai-gaˊ kiung-ha loiˇ",
-    }, {
-      漢字: "大家 共下 來",
-      臺灣客話: "Tai-gaˊ kiung-ha loiˇ",
-    }];
+    const 綜合標音 = 一標音.concat(一標音);
     const expectResult = {
       漢字: "大家共下來\n大家共下來",
       臺羅: "Tai-gaˊ kiung-ha loiˇ\nTai-gaˊ kiung-ha loiˇ",
-      漢字臺羅: "大家共下來\nTai-gaˊ kiung-ha loiˇ\n大家共下來\nTa̍k-ke tsò-hué lâi-tshit-thô ！",
+      漢字臺羅: "大家共下來\nTai-gaˊ kiung-ha loiˇ\n大家共下來\nTai-gaˊ kiung-ha loiˇ",
     };
     expect(計算複製內容(綜合標音)).to.eql(expectResult);
   });
