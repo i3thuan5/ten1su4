29bda78fbbb2ff44d69ad91d8f19d93d00e1424b
a8568730 <a8568730@hotmail.com>
2017-05-16 14:13:54 +0800

    查詢的action加上腔口

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,41 +2,47 @@ import superagent from 'superagent-bluebird-promise';
 import {
   REQUEST_HANLO,
   RECIEVE_HANLO,
-  RECIEVE_ERROR_HANLO
+  RECIEVE_ERROR_HANLO,
+  SWITCH_ACCENT
 } from './action.type';
 import { 後端網址, 標漢字音標 } from '../後端網址';
 
-export const 請求遠端查詢 = 語句 => ({
+export const 請求遠端查詢 = (語句, 腔口) => ({
   type: REQUEST_HANLO,
   語句,
+  腔口
 });
 
-export const 收到遠端查詢 = (語句, body) => ({
+export const 收到遠端查詢 = (語句, 腔口, body) => ({
   type: RECIEVE_HANLO,
   語句,
+  腔口,
   '查詢結果': body,
 });
 
-export const 遠端查詢發生錯誤 = (語句) => ({
+export const 遠端查詢發生錯誤 = (語句, 腔口) => ({
   type: RECIEVE_ERROR_HANLO,
   語句,
+  腔口
 });
 
-export const 遠端查詢 = 語句 => dispatch => {
-  dispatch(請求遠端查詢(語句));
+export const 遠端查詢 = (語句, 腔口) => dispatch => {
+  dispatch(請求遠端查詢(語句, 腔口));
   return superagent
     .get(後端網址 + 標漢字音標)
     .query({
-        '查詢腔口': '四縣腔',
+        '查詢腔口': 腔口,
         '查詢語句': 語句.trim(),
       })
-    .then(({ body }) => dispatch(收到遠端查詢(語句, body)))
-    .catch(error => dispatch(遠端查詢發生錯誤(語句)));
+    .then(({ body }) => dispatch(收到遠端查詢(語句, 腔口, body)))
+    .catch(error => dispatch(遠端查詢發生錯誤(語句, 腔口)));
 };
 
-export const 是否可以請求查詢 = (state, 語句) => {
-  if (state.查詢.語句 === 語句 ||
-    state.查詢.正在查詢)
+export const 是否可以請求查詢 = (state, 語句, 腔口) => {
+  if ((state.查詢.語句 === 語句 && 
+    state.查詢.腔口 === 腔口) || 
+    state.查詢.正在查詢
+  )
   {
     return false;
   }
@@ -44,8 +50,8 @@ export const 是否可以請求查詢 = (state, 語句) => {
   return true;
 };
 
-export const 查詢語句 = 語句 => (dispatch, getState) => {
-  if (是否可以請求查詢(getState(), 語句)) {
-    return dispatch(遠端查詢(語句));
+export const 查詢語句 = (語句, 腔口) => (dispatch, getState) => {
+  if (是否可以請求查詢(getState(), 語句, 腔口)) {
+    return dispatch(遠端查詢(語句, 腔口));
   }
 };
--- test/actions/index.spec.js
+++ test/actions/index.spec.js
@@ -6,60 +6,94 @@ import thunk from 'redux-thunk';
 import {
   REQUEST_HANLO,
   RECIEVE_HANLO,
-  RECIEVE_ERROR_HANLO
+  RECIEVE_ERROR_HANLO,
+  SWITCH_ACCENT
 } from '../../src/actions/action.type';
-import { 是否可以請求查詢, 遠端查詢 } from '../../src/actions/';
+import { 
+  是否可以請求查詢, 
+  遠端查詢,
+  切換腔口
+ } from '../../src/actions/';
 import { 後端網址, 標漢字音標 } from '../../src/後端網址';
 
 const middlewares = [thunk];
 const mockStore = configureMockStore(middlewares);
 
 describe('Action', () => {
+  
+  const 腔口 = '四縣腔';
+
   afterEach(()=> {
     nock.cleanAll();
   });
 
-  it('passes valid search', function () {
-      const 語句 = '下一句';
+  it('查新語句', function () {
+      const 下一句 = '下一句';
       const store = mockStore({
         查詢: {
           語句: '大家共下來',
+          腔口: 腔口,
           正在查詢: false,
           查詢結果: {},
         },
       });
-      expect(是否可以請求查詢(store.getState(), 語句)).to.be.true;
+      expect(是否可以請求查詢(
+        store.getState(), 下一句, 腔口
+      )).to.be.true;
+  });
+
+  it('查新腔調', function () {
+      const 同一句 = '大家共下來';
+      const 下一腔口 = '海陸腔';
+      const store = mockStore({
+        查詢: {
+          語句: 同一句,
+          腔口: 腔口,
+          正在查詢: false,
+          查詢結果: {},
+        },
+      });
+      expect(是否可以請求查詢(
+        store.getState(), 同一句, 下一腔口
+      )).to.be.true;
     });
 
-  it('stops invalid search', ()=> {
-    const 語句 = '下一句';
+  it('查詢中，擋掉新查詢', ()=> {
+    const 下一句 = '下一句';
     const store = mockStore({
       查詢: {
         語句: '大家共下來',
+        腔口: 腔口,
         正在查詢: true,
         查詢結果: {},
       },
     });
-    expect(是否可以請求查詢(store.getState(), 語句)).to.be.false;
+    expect(是否可以請求查詢(
+      store.getState(), 下一句, 腔口
+    )).to.be.false;
   });
 
   it('stops same search', ()=> {
-    const 語句 = '大家共下來';
+    const 同一句 = '大家共下來';
+    const 同一腔口 = 腔口;
     const store = mockStore({
       查詢: {
-        語句: '大家共下來',
+        語句: 同一句,
+        腔口: 同一腔口,
         正在查詢: false,
         查詢結果: {},
       },
     });
-    expect(是否可以請求查詢(store.getState(), 語句)).to.be.false;
+    expect(是否可以請求查詢(
+      store.getState(), 同一句, 同一腔口
+    )).to.be.false;
   });
 
   it('creates RECIEVE_HANLO when fetching data is done', () => {
     nock(後端網址)
     .get('/' + 標漢字音標)
     .query({
-      '查詢腔口': '四縣腔',
+      '查詢腔口': 腔口,
       '查詢語句': '大家共下來',
     })
     .reply(200, {
@@ -73,13 +107,13 @@ describe('Action', () => {
 
     const store = mockStore({
       查詢: {
-        '查詢結果': {},
+        查詢結果: {}
       },
     });
 
     const expectActions = [
-      { type: REQUEST_HANLO, '語句': '大家共下來' },
-      { type: RECIEVE_HANLO, '語句': '大家共下來',
+      { type: REQUEST_HANLO, '語句': '大家共下來', 腔口: 腔口 },
+      { type: RECIEVE_HANLO, '語句': '大家共下來', 腔口: 腔口,
         '查詢結果': {
           '分詞': '大-家｜tai-gaˊ 共-下｜kiung-ha 來｜loiˇ',
           '綜合標音': [{
@@ -90,7 +124,7 @@ describe('Action', () => {
         }, 
     },];
 
-    return store.dispatch(遠端查詢('大家共下來'))
+    return store.dispatch(遠端查詢('大家共下來', 腔口))
       .then(()=> {
         expect(store.getActions()).to.eql(expectActions);
       });
@@ -100,7 +134,7 @@ describe('Action', () => {
     nock(後端網址)
     .get('/' + 標漢字音標)
     .query({
-      '查詢腔口': '四縣腔',
+      '查詢腔口': 腔口,
       '查詢語句': '大家\n來',
     })
     .reply(200, {
@@ -123,8 +157,8 @@ describe('Action', () => {
     });
 
     const expectActions = [
-      { type: REQUEST_HANLO, '語句': '大家\n來' },
-      { type: RECIEVE_HANLO, '語句': '大家\n來',
+      { type: REQUEST_HANLO, '語句': '大家\n來', 腔口: 腔口 },
+      { type: RECIEVE_HANLO, '語句': '大家\n來', 腔口: 腔口,
         '查詢結果': {
           '分詞': '大-家｜tai-gaˊ 來｜loiˇ',
           '綜合標音': [{
@@ -139,7 +173,7 @@ describe('Action', () => {
         },
       },];
 
-    return store.dispatch(遠端查詢('大家\n來'))
+    return store.dispatch(遠端查詢('大家\n來', 腔口))
       .then(()=> {
         expect(store.getActions()).to.eql(expectActions);
       });
@@ -149,7 +183,7 @@ describe('Action', () => {
     nock(後端網址)
     .get('/' + 標漢字音標)
     .query({
-      '查詢腔口': '四縣腔',
+      '查詢腔口': 腔口,
       '查詢語句': '大家共下來',
     })
     .replyWithError('你糗了你！');
@@ -161,10 +195,10 @@ describe('Action', () => {
     });
 
     const expectActions = [
-      { type: REQUEST_HANLO, '語句': '大家共下來' },
-      { type: RECIEVE_ERROR_HANLO, '語句': '大家共下來' },];
+      { type: REQUEST_HANLO, '語句': '大家共下來', 腔口: 腔口 },
+      { type: RECIEVE_ERROR_HANLO, '語句': '大家共下來', 腔口: 腔口 },];
 
-    return store.dispatch(遠端查詢('大家共下來'))
+    return store.dispatch(遠端查詢('大家共下來', 腔口))
       .then(()=> {
         expect(store.getActions()).to.eql(expectActions);
       });
