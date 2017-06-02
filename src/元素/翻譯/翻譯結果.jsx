0b566390bd0f8001a8cf927a081c37c107647b2a
a8568730 <a8568730@hotmail.com>
2017-06-02 21:06:10 +0800

    翻譯結果的PropType檢查羅馬字

--- src/元素/翻譯/翻譯結果.jsx
+++ src/元素/翻譯/翻譯結果.jsx
@@ -72,7 +72,7 @@ class 翻譯結果 extends React.Component {
   綜合標音: PropTypes.arrayOf(PropTypes.shape({
     分詞: PropTypes.string.isRequired,
     漢字: PropTypes.string.isRequired,
-    臺灣客話: PropTypes.string.isRequired,
+    羅馬字: PropTypes.string.isRequired,
   })),
 };
 
