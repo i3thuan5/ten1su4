79799505809d9d4a468966f442f81ad7c3cd8497
a8568730 <a8568730@hotmail.com>
2017-06-09 18:31:58 +0800

    由webpack讀取taupahji.config.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,36 +1,9 @@
-import path from "path";
-
-const funcs = {};
-
-function getConfigPath() {
-  const CONFIG_FILE = "taupahji.config.js";
-  // read config from root of working directory
-  return path.join(path.resolve(), CONFIG_FILE);
-}
-
-function loadConfigJS(filePath) {
-  try {
-    const config = require(filePath);
-  } catch (err) {
-    err.message = "taupahji.config.js not found.";
-    throw err;
-  }
-  return config;
-}
-
-funcs.getConfigPath = getConfigPath;
-funcs.loadConfigJS = loadConfigJS;
-
-const config = loadConfigJS(getConfigPath());
-
 const configGenerator = {
-  專案: () => (config.專案),
-  全部腔口: () => (config.腔口),
-  預設腔口: () => (config.腔口[0]),
-  範例查詢: () => (config.範例查詢),
-  頁尾連結: () => (config.頁尾連結),
+  專案: () => tensuConfig.專案,
+  全部腔口: () => tensuConfig.腔口,
+  預設腔口: () => tensuConfig.腔口[0],
+  範例查詢: () => tensuConfig.範例查詢,
+  頁尾連結: () => tensuConfig.頁尾連結,
 };
-
-export { funcs as FUNC };
 
 export default configGenerator;
