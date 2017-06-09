de97beea6046f5c5a08942807ef7da8e2c576e6f
a8568730 <a8568730@hotmail.com>
2017-06-09 19:29:23 +0800

    讓試驗可以吃到alias。 安裝babel-plugin-webpack-alias和eslint-import-resolver-webpack

--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,11 @@
+import config from "tensuConfig";
+
 const configGenerator = {
-  專案: () => tensuConfig.專案,
-  全部腔口: () => tensuConfig.腔口,
-  預設腔口: () => tensuConfig.腔口[0],
-  範例查詢: () => tensuConfig.範例查詢,
-  頁尾連結: () => tensuConfig.頁尾連結,
+  專案: () => config.專案,
+  全部腔口: () => config.腔口,
+  預設腔口: () => config.腔口[0],
+  範例查詢: () => config.範例查詢,
+  頁尾連結: () => config.頁尾連結,
 };
 
 export default configGenerator;
