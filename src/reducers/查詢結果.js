c7bd4243de0c276ef91b2f21dc13dd17e31f2465
a8568730 <a8568730@hotmail.com>
2018-09-06 18:30:10 +0800

    多元書寫的臺羅key

--- src/reducers/查詢結果.js
+++ src/reducers/查詢結果.js
@@ -3,22 +3,29 @@ import {
   RECIEVE_ERROR_HANLO,
 } from "../actions/action.type";
 
+
+
 export const 正規化綜合標音 = (綜合標音 = []) => {
+  //統一api回傳的標音鍵值
   if (!綜合標音 || 綜合標音.length === 0) {
     return [];
   }
-  const first = 綜合標音[0];
-  const keys = Object.keys(first);
+  const 第一句 = 綜合標音[0];
+  const keys = Object.keys(第一句);
   const newKeys = keys.filter(x => (
-    x !== "臺羅閏號調" && x !== "臺灣客話"
+    x !== "臺羅閏號調" && x !== "臺灣客話" && x !== "臺羅"
   ));
   return 綜合標音.map((t) => {
     const result = {};
-    result.羅馬字 = t.臺羅閏號調 || t.臺灣客話;
+    //統一叫羅馬字
+    result.羅馬字 = (t.臺羅閏號調 || t.臺羅) || t.臺灣客話;
+    //其他鍵值維持原鍵名
     newKeys.forEach((k) => { result[k] = t[k]; });
     return result;
   });
 };
+
+
 
 const 初始state = () => ({
   結果語句: "",
